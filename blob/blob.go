d15a3f478ea57c5a32e36c1a616da67475da2ddf
Jonathan Hall <flimzy@flimzy.com>
2016-02-13 16:11:04 +0100

    Make Blob an interface

--- blob/blob.go
+++ blob/blob.go
@@ -16,8 +16,17 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+type Blob interface {
+	IsClosed() bool
+	Size() uint
+	Type() string
+	Close()
+	Slice(int,int,string) Blob
+	Bytes() []byte
+}
+
 // Blob wraps a js.Object
-type Blob struct {
+type BasicBlob struct {
 	js.Object
 }
 
@@ -28,42 +37,42 @@ type Options struct {
 
 // New returns a newly created Blob object whose content consists of the
 // concatenation of the array of values given in parameter.
-func New(parts []interface{}, opts Options) *Blob {
+func New(parts []interface{}, opts Options) Blob {
 	blob := js.Global.Get("Blob").New(parts, opts)
-	return &Blob{*blob}
+	return &BasicBlob{*blob}
 }
 
 // IsClosed returns true if the Close() method (or the underlying JavaScript
 // Blobl.close() method) has been called on the blob. Closed blobs can not be
 // read.
-func (b *Blob) IsClosed() bool {
+func (b *BasicBlob) IsClosed() bool {
 	return b.Get("isClosed").Bool()
 }
 
 // Size returns the size, in bytes, of the data contained in the Blob object.
-func (b *Blob) Size() int {
-	return b.Get("size").Int()
+func (b *BasicBlob) Size() uint {
+	return uint(b.Get("size").Uint64())
 }
 
 // Type returns a string indicating the MIME type of the data contained in
 // the Blob. If the type is unknown, this string is empty.
-func (b *Blob) Type() string {
+func (b *BasicBlob) Type() string {
 	return b.Get("type").String()
 }
 
 // Close closes the blob object, possibly freeing underlying resources.
-func (b *Blob) Close() {
+func (b *BasicBlob) Close() {
 	b.Call("close")
 }
 
 // Slice returns a new Blob object containing the specified range of bytes of the source Blob.
-func (b *Blob) Slice(start, end int, contenttype string) *Blob {
+func (b *BasicBlob) Slice(start, end int, contenttype string) Blob {
 	newBlob := b.Call("slice", start, end, contenttype)
-	return &Blob{*newBlob}
+	return &BasicBlob{*newBlob}
 }
 
 // Bytes returns a slice of the contents of the Blob.
-func (b *Blob) Bytes() []byte {
+func (b *BasicBlob) Bytes() []byte {
 	fileReader := js.Global.Get("FileReader").New()
 	var wg sync.WaitGroup
 	var buf []byte
